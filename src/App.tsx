b3c9f1338774cf03785392c63b0fe70da3914032
yuvrajalr <132020646+codewyuu@users.noreply.github.com>
2025-04-04 17:14:56 +0530

    Revert "added chatbot "

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -5,7 +6,6 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { LanguageProvider } from "@/contexts/LanguageContext";
 import { ThemeProvider } from "@/contexts/ThemeContext";
-import ChatBot from "@/components/ChatBot";
 import Index from "./pages/Index";
 import About from "./pages/About";
 import Events from "./pages/Events";
@@ -38,7 +38,6 @@ const App = () => (
               <Route path="*" element={<NotFound />} />
             </Routes>
           </BrowserRouter>
-          <ChatBot />
         </TooltipProvider>
       </LanguageProvider>
     </ThemeProvider>
